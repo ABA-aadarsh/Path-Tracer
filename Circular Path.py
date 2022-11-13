eacbdae7ed8e1756ba28b7b88138c9ff7e3e86f6
Aadarsh <109805574+ABA-aadarsh@users.noreply.github.com>
2022-11-13 09:58:37 +0545

    Update Circular Path.py

--- Circular Path.py
+++ Circular Path.py
@@ -4,12 +4,12 @@ import time
 import math
 global r, pi
 r=int(input("Enter the radius: "))
-def open_mspaint():
-    auto.hotkey("win","r")
-    auto.write("mspaint")
-    auto.press("enter")
-    time.sleep(3)
-open_mspaint()
+# def open_mspaint():
+#     auto.hotkey("win","r")
+#     auto.write("mspaint")
+#     auto.press("enter")
+#     time.sleep(3)
+# open_mspaint()
 pi=math.pi
 def x(theta):
     x_position=r*math.cos(pi*theta/180)
@@ -21,11 +21,11 @@ centre=[674, 384] # Centre of the circle
 #now we need to move the mouse in a circular path
 # the locus of the mouse for circular moverment will be (rcos(theta)+h,rsin(theta)+k) where (h,k) is the centre of the circle and r is radius
 theta=0
-auto.mouseDown(x(theta)+centre[0], y(theta)+centre[1], button="left")
+# auto.mouseDown(x(theta)+centre[0], y(theta)+centre[1], button="left")
 while theta>-370:
     auto.moveTo(x(theta)+centre[0],y(theta)+centre[1],0)
     theta-=3
-auto.mouseUp()
+# auto.mouseUp()
 position=auto.position()
 auto.click(position.x, position.y, 1, 0.1, button="left")
 exit()
